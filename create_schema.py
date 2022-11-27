c3f36efcef13d8773eea81fe6eb1141cff737c03
Injector1 <injector005@gmail.com>
2022-11-27 19:42:32 +0500

    feat: implemented docker
    * added Dockerfile, dockerignore
    * added script for application bootstrapping
    * added env variables and implemented env example

--- create_schema.py
+++ create_schema.py
@@ -1,9 +1,18 @@
+import os
+
 from tortoise import Tortoise, run_async
+
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_DB_NAME = os.getenv('POSTGRES_DB_NAME')
 
 
 async def connect_to_database():
     await Tortoise.init(
-        db_url='',
+        db_url=f'postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB_NAME}?schema'
+               f'=public',
         modules={'models': ['app.features.telegraf.models']}
     )
 
@@ -11,6 +20,7 @@ async def connect_to_database():
 async def database_creation():
     await connect_to_database()
     await Tortoise.generate_schemas()
+
 
 if __name__ == '__main__':
     run_async(database_creation())
