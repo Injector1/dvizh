a7757bc3622a52a2ae517a749310deb7d8b4f79f
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2022-11-27 16:29:39 +0500

    fix: little fix

--- app/features/parser/service.py
+++ app/features/parser/service.py
@@ -3,23 +3,23 @@ from .parsers.sportsru_parser import SportsRUParser
 import schedule
 
 
-METHOD_BY_SITE = {'sports.ru': SportsRUParser}
-
-
 class ParserService:
     def __init__(self, sportsru_parser: SportsRUParser):
         self.sportsru_parser = sportsru_parser
+        self.method_by_site = {'sports.ru': self.sportsru_parser}
 
     def update_all_telegraph_articles(self):
         print('start')
         for name in ARTICLES_BY_NAME.keys():
             print(f'Updating team {name}')
             for site, tag in TAGS[name].items():
-                if site in METHOD_BY_SITE.keys():
-                    article_info = self.sportsru_parser.get_markdown_view(
+                if site in self.method_by_site.keys():
+                    article_info = self.method_by_site[site].get_markdown_view(
                         self.sportsru_parser.parse(tag),
                         name
                     )
+                    if ARTICLES_BY_NAME[name][0] != article_info:
+                        print('Change!')
                     ARTICLES_BY_NAME[name][0] = article_info
         print('done')
 
